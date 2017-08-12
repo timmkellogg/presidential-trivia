71a943411ef11fb315e29fcf86a526f74d2b6e99
Tim Kellogg <timmkellogg@gmail.com>
2017-08-12 13:23:58 -0400

    pushed initial files

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+/*
+Click to start (show where timer is)
+
+Start timer
+
+On click or timeout, highlight correct answer green
+If clicked answer is incorrect, highlight red
+wait X seconds
+New question
+
+After all questions displated, show score screen
